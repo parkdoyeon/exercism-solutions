38b69e92767037ad3c6642b25a4850f9cb9b0a75
Park Doyeon <park.doyeonn@gmail.com>
2020-01-30 10:37:36 +0900

    add list-ops (not solved)

---
+++ elixir/list-ops/lib/list_ops.ex
@@ -0,0 +1,45 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    _len(l)
+  end
+
+  def _len([h | t]) do
+    1+_len(t)
+  end
+
+  def _len([]) do
+    0
+  end
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, (any, acc -> acc)) :: acc
+  def reduce(l, acc, f) do
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+  end
+end
