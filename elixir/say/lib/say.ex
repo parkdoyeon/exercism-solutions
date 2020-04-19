cf2ed76ed3ba881bd7f7674d07902da7f63b5a1c
박도연 <park.doyeonn@gmail.com>
2020-04-19 17:59:19 +0900

    add say not solved

---
+++ elixir/say/lib/say.ex
@@ -0,0 +1,24 @@
+defmodule Say do
+  @doc """
+  Translate a positive integer into English.
+  """
+  @spec in_english(integer) :: {atom, String.t()}
+  def in_english(number) do
+  end
+
+  def normal_num(number) do
+    case number do
+      1 -> "one"
+      2 -> "two"
+      3 -> "three"
+      4 -> "four"
+      5 -> "five"
+      6 -> "six"
+      7 -> "seven"
+      8 -> "eight"
+      9 -> "nine"
+      0 -> ""
+    end
+
+  end
+end
